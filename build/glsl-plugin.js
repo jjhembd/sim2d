6a59579a22c44704051466ff306e47e43f0bc7f4
Presbyterian Librarian <jjhembd@gmail.com>
2019-04-08 14:04:12 -0500

    Standardized directory structure and build script

--- build/glsl-plugin.js
+++ build/glsl-plugin.js
@@ -1,9 +1,8 @@
-import resolve from 'rollup-plugin-node-resolve';
-
 // Plugin for .glsl files, roughly following the rollup.config.js in three.js
 // with some guidance from github.com/vwochnik/rollup-plugin-glsl
 // and rollupjs.org/guide/en#plugins-overview
-function glsl() {
+// TODO: replace with glslify and rollup-plugin-glslify ?
+export function glsl() {
   return {
     transform( source, id ) { // Too much indentation? Follows three.js...
       // Confirm filename extension is .glsl -- follows three.js
@@ -20,17 +19,3 @@ function glsl() {
     }
   };
 }
-
-export default {
-  input: 'src/main.js',
-  plugins: [
-    glsl(),
-    resolve(),
-  ],
-  output: {
-    file: 'build/sim2d.bundle.js',
-    //sourcemap: 'inline',
-    format: 'esm',
-    name: 'sim2d'
-  }
-};
